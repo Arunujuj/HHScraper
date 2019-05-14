51b45e19806045d6d00ddb1e21f5e04d0728e7df
Arunujuj <arunujuj@gmail.com>
2019-05-14 16:26:09 +0200

    Init
    First step done. Can get direct video url from viewing page. Download or share is possible.

---
+++ HHScraper/HentaiHavenScraper.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHScraper
+{
+    public class HentaiHavenScraper
+    {
+
+        public string GetHHDirectVideoURL(string baseVideoUrl)
+        {
+            string resultURL = "";
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(Tools.GetHTML(baseVideoUrl));
+
+            var nodes = doc.DocumentNode;
+
+            foreach (HtmlNode metaTag in doc.DocumentNode.Descendants("video"))
+            {
+                resultURL = Tools.GetSRC(metaTag.InnerHtml);
+            }
+            return resultURL;
+        }
+
+    }
+}
---
+++ HHScraperTester/Program.cs
@@ -0,0 +1,33 @@
+﻿using HHScraper;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHScraperTester
+{
+
+
+    // EXAMPLE LINK: https://hentaihaven.org/koi-maguwai-episode-1/
+    // THIS APPLICATION WILL RETURN THE DIRECT VIDEO URL
+    // (THE DIRECT VIDEO URL CAN BE DOWNLOADED OR SHARED, NOT LIKE THE VIEWING PAGE)
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HentaiHavenScraper hhs = new HentaiHavenScraper();
+            string inputUrl = Console.ReadLine();
+            string url = hhs.GetHHDirectVideoURL(inputUrl);
+            if (url != "-")
+                Process.Start(url);
+            else
+                Console.WriteLine("cannot open url");
+
+            Console.ReadLine();
+        }
+    }
+}
