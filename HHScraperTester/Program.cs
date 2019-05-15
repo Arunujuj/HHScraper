c7743e552d51a1cc039c6f9548323e7d8b591f42
Arunujuj <arunujuj@gmail.com>
2019-05-15 10:31:52 +0200

    update

--- HHScraperTester/Program.cs
+++ HHScraperTester/Program.cs
@@ -62,6 +62,7 @@ namespace HHScraperTester
                         }
                     }
                     catch (Exception) { }
+                    Console.Clear();
                 }
                 
             }
