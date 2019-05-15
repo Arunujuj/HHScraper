d3fc1fddbcfe755270583bc4aa6c70df433fedab
Arunujuj <arunujuj@gmail.com>
2019-05-15 10:15:28 +0200

    hehe
    Can scrape every series and every episode. Running the tester application returns a /hh/ directory which contains every series with description and all episode direct links + the thumbnails. Artificial wait in between the scrapes to not get banned from requesting. (to minimize requests, do "GetSeriesList()" once and save it to "series.txt" so you dont have to request the list all the time.

--- HHScraper/HentaiHavenScraper.cs
+++ HHScraper/HentaiHavenScraper.cs
@@ -10,13 +10,20 @@ namespace HHScraper
 {
     public class HentaiHavenScraper
     {
+        private HtmlDocument LoadHTMLSite(string url)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(Tools.GetHTML(url));
+            return doc;
+        }
+
+
 
         public string GetHHDirectVideoURL(string baseVideoUrl)
         {
             string resultURL = "";
 
-            var doc = new HtmlDocument();
-            doc.LoadHtml(Tools.GetHTML(baseVideoUrl));
+            var doc = LoadHTMLSite(baseVideoUrl);
 
             var nodes = doc.DocumentNode;
 
@@ -26,6 +33,65 @@ namespace HHScraper
             }
             return resultURL;
         }
+
+
+
+        public string[] GetSeriesList()
+        {
+            string baseUrl = "https://hentaihaven.org/pick-your-series/";
+            var doc = LoadHTMLSite(baseUrl);
+            var nodes = doc.DocumentNode.SelectNodes("//*[contains(@class,'category_alphabet_title_link')]");
+            string[] names = new string[nodes.Count];
+
+            for(int i = 0; i<nodes.Count; i++)
+            {
+                string seriesName = nodes[i].InnerHtml;
+                seriesName = seriesName.Replace("...", string.Empty);
+                seriesName = seriesName.Replace("!", string.Empty);
+                names[i] = seriesName.Replace(" ", "-");
+            }
+            return names;
+        }
+
+        public string GetSeriesDescription(string seriesName)
+        {
+            string baseUrl = "https://hentaihaven.org/series/" + seriesName;
+            string description = "";
+
+            var doc = LoadHTMLSite(baseUrl);
+
+            var nodes = doc.DocumentNode.SelectNodes("//*[contains(@class,'archive-meta category-meta')]");
+            description = nodes.FirstOrDefault().InnerText;
+            return description;
+
+        }
+
+        public string GetThumbnail(string baseVideoUrl)
+        {
+            string thumbnailURL = "";
+
+            var doc = LoadHTMLSite(baseVideoUrl);
+
+            foreach (HtmlNode metaTag in doc.DocumentNode.Descendants("video"))
+            {
+                thumbnailURL = metaTag.GetAttributeValue("poster", "-");
+            }
+            return thumbnailURL;
+        }
+
+        public string[] GetEpisodeListFromSeries(string seriesName)
+        {
+            string baseUrl = "https://hentaihaven.org/series/" + seriesName; 
+            var doc = LoadHTMLSite(baseUrl);
+            var nodes = doc.DocumentNode.SelectNodes("//*[contains(@class,'brick-title')]");
+            string[] episodes = new string[nodes.Count];
+            for(int i = 0; i < nodes.Count; i++)
+            {
+                episodes[i] = nodes[i].GetAttributeValue("href", "-");
+            }
+            return episodes;
+        }
+
 
     }
 }
--- HHScraperTester/Program.cs
+++ HHScraperTester/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HHScraperTester
@@ -13,19 +14,46 @@ namespace HHScraperTester
     // EXAMPLE LINK: https://hentaihaven.org/koi-maguwai-episode-1/
     // THIS APPLICATION WILL RETURN THE DIRECT VIDEO URL
     // (THE DIRECT VIDEO URL CAN BE DOWNLOADED OR SHARED, NOT LIKE THE VIEWING PAGE)
-
-
+    
     class Program
     {
+
+        private static Random rnd = new Random();
         static void Main(string[] args)
         {
             HentaiHavenScraper hhs = new HentaiHavenScraper();
-            string inputUrl = Console.ReadLine();
-            string url = hhs.GetHHDirectVideoURL(inputUrl);
-            if (url != "-")
-                Process.Start(url);
-            else
-                Console.WriteLine("cannot open url");
+
+            string[] seriesNames = System.IO.File.ReadAllLines("series.txt");
+            //System.IO.File.WriteAllLines("series.txt", seriesNames);
+
+
+            foreach(string series in seriesNames)
+            {
+                string directorySeriesName = series.Replace(":", string.Empty);
+                Thread.Sleep(5000);
+
+
+                System.IO.Directory.CreateDirectory("hh/" + directorySeriesName);
+
+
+                Console.WriteLine("===== " + series + " =====");
+                System.IO.File.WriteAllText("hh/" + directorySeriesName + "/links.txt", "https://hentaihaven.org/series/" + series);
+
+                string seriesDescription = hhs.GetSeriesDescription(series);
+                System.IO.File.WriteAllText("hh/" + directorySeriesName + "/desc.txt", seriesDescription);
+
+                int epiCounter = 1;
+                foreach(var episode in hhs.GetEpisodeListFromSeries(series))
+                {
+                    Thread.Sleep(2000);
+                    string episodeThumbnail = hhs.GetThumbnail(episode);
+                    System.IO.File.AppendAllText("hh/" + directorySeriesName + "/links.txt", System.Environment.NewLine + "thumbnail: " + episodeThumbnail);
+                    string directVideo = hhs.GetHHDirectVideoURL(episode);
+                    System.IO.File.AppendAllText("hh/" + directorySeriesName + "/links.txt", System.Environment.NewLine + "video: " + directVideo);
+                    Console.WriteLine(epiCounter + ". " + directVideo);
+                    epiCounter++;
+                }
+            }
 
             Console.ReadLine();
         }
